75fce5eeb15b0d7005ea97ffb9e5986e0512eb3b
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-03-17 00:16:17 -0300

    :sparkles: feat: Adiciona implementação inicial do Hilt.

---
+++ app/src/main/java/com/guilhermereisapps/guiadelivros/ReaderApplication.kt
@@ -0,0 +1,7 @@
+package com.guilhermereisapps.guiadelivros
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication : Application() {}
