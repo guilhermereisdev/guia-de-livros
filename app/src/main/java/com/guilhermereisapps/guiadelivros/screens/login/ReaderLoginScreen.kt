1e0be315697f19b65494fb3bb52a6dd507167858
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-03-19 04:42:37 -0300

    :sparkles: feat: Adiciona a Logo e o UserForm na tela de Login.

--- app/src/main/java/com/guilhermereisapps/guiadelivros/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/guilhermereisapps/guiadelivros/screens/login/ReaderLoginScreen.kt
@@ -1,10 +1,82 @@
 package com.guilhermereisapps.guiadelivros.screens.login
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.guilhermereisapps.guiadelivros.components.EmailInput
+import com.guilhermereisapps.guiadelivros.components.PasswordInput
+import com.guilhermereisapps.guiadelivros.components.ReaderLogo
 
 @Composable
 fun ReaderLoginScreen(navController: NavController) {
-    Text(text = "Login Screen")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            ReaderLogo()
+            UserForm()
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, password -> }
+) {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+    val modifier = Modifier
+//        .height(250.dp)
+        .background(MaterialTheme.colorScheme.background)
+        .verticalScroll(rememberScrollState())
+
+    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        EmailInput(
+            emailState = email,
+            enabled = !loading,
+            onAction = KeyboardActions {
+                passwordFocusRequest.requestFocus()
+            },
+        )
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            },
+        )
+    }
 }
