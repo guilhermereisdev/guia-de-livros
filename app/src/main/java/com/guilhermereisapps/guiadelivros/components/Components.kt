73e49290c1e508fe276723b69df4e332d63a8cc7
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-04-19 03:00:56 -0300

    :recycle: refactor: Transfere a AppBar para o arquivo de Components.kt para ser reutilizado.

--- app/src/main/java/com/guilhermereisapps/guiadelivros/components/Components.kt
+++ app/src/main/java/com/guilhermereisapps/guiadelivros/components/Components.kt
@@ -1,28 +1,40 @@
 package com.guilhermereisapps.guiadelivros.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -31,6 +43,10 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.guilhermereisapps.guiadelivros.navigation.ReaderScreens
 
 @Preview
 @Composable
@@ -163,6 +179,61 @@ fun MenuItem(
         onClick = {
             showMenu.value = false
             onClick()
+        },
+    )
+}
+
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    title: String = "Actual Screen",
+    icon: ImageVector? = null,
+    showProfile: Boolean = false,
+    navController: NavController = rememberNavController(),
+    onBackArrowClicked: () -> Unit = {}
+) {
+    val showMenu = remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(
+                        imageVector = Icons.Default.Book,
+                        contentDescription = "Profile icon",
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .scale(1.2f)
+                            .padding(end = 12.dp),
+                    )
+                }
+                if (icon != null) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = "Arrow Back",
+                        modifier = Modifier.clickable { onBackArrowClicked.invoke() },
+                    )
+                }
+                Text(text = title)
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Yellow),
+        actions = {
+            IconButton(onClick = { showMenu.value = !showMenu.value }) {
+                Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "Menu")
+            }
+            DropdownMenu(expanded = showMenu.value, onDismissRequest = { showMenu.value = false }) {
+                MenuItem(text = "Minha conta", showMenu = showMenu)
+                MenuItem(text = "Estatísticas", showMenu = showMenu) {
+                    navController.navigate(ReaderScreens.StatsScreen.name)
+                }
+                MenuItem(text = "Sair", showMenu = showMenu) {
+                    FirebaseAuth.getInstance().signOut().run {
+                        navController.navigate(ReaderScreens.LoginScreen.name)
+                    }
+                }
+            }
         },
     )
 }
