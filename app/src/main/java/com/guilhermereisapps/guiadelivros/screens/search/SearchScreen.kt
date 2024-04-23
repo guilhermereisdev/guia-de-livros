e58efb1b8a9b10586bd3ab95b6261233f5c28440
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-04-22 23:59:10 -0300

    :recycle: refactor: Adiciona o SingleBookCard no arquivo de Components.kt

--- app/src/main/java/com/guilhermereisapps/guiadelivros/screens/search/SearchScreen.kt
+++ app/src/main/java/com/guilhermereisapps/guiadelivros/screens/search/SearchScreen.kt
@@ -1,44 +1,30 @@
 package com.guilhermereisapps.guiadelivros.screens.search
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.rememberAsyncImagePainter
 import com.guilhermereisapps.guiadelivros.components.AppBar
 import com.guilhermereisapps.guiadelivros.components.InputField
+import com.guilhermereisapps.guiadelivros.components.SingleBookCard
 import com.guilhermereisapps.guiadelivros.model.Book
 
 @Preview
@@ -85,60 +71,7 @@ fun BookList(navController: NavController) {
         modifier = Modifier.fillMaxSize(),
     ) {
         items(items = listOfBooks) { book ->
-            BookRow(book = book, navController = navController)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun BookRow(
-    book: Book =
-        Book("01", "Harry Potter e a Pedra Filosofal", "Aquela lá", "Menino bruxo"),
-    navController: NavController = rememberNavController(),
-) {
-    Card(
-        modifier = Modifier
-            .clickable { }
-            .fillMaxWidth()
-            .height(150.dp)
-            .padding(horizontal = 8.dp, vertical = 4.dp),
-        shape = RoundedCornerShape(8.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
-    ) {
-        Row(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            val imageUrl =
-                "http://books.google.com/books/content?id=aGMfCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            Image(
-                painter = rememberAsyncImagePainter(model = imageUrl),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxHeight()
-//                    .aspectRatio(1f)
-                    .width(100.dp)
-//                    .clip(
-//                        shape = RoundedCornerShape(
-//                            topStart = 8.dp,
-//                            bottomStart = 8.dp,
-//                        )
-//                    ),
-                        ,
-                contentScale = ContentScale.FillHeight // Preencher verticalmente sem distorção horizontal
-            )
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-            ) {
-                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
-                Text(
-                    text = "Author(s): " + book.authors.toString(), overflow = TextOverflow.Clip,
-                    style = MaterialTheme.typography.bodySmall
-                )
-                // TODO: adicionar mais campos aqui
-            }
+            SingleBookCard(book = book, navController = navController)
         }
     }
 }
